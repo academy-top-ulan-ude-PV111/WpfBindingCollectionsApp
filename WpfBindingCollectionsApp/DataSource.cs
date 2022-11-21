d78d0222e30aa761700539cfa8cb8a2afc39421c
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-11-21 16:30:14 +0300

    Добавьте файлы проекта.

---
+++ WpfBindingCollectionsApp/DataSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfBindingCollectionsApp
+{
+    internal class DataSource
+    {
+        private ICollection<int> nonNotifiableCollection = new List<int> { 1, 2, 3 };
+        private ICollection<int> notifiableCollection = new ObservableCollection<int> { 1, 2, 3 };
+
+        public IEnumerable<int> NonNotifiableCollection { get => nonNotifiableCollection; }
+        public IEnumerable<int> NotifiableCollection { get => notifiableCollection; }
+
+
+        public void AddNonNotifiableCollection()
+        {
+            nonNotifiableCollection.Add(nonNotifiableCollection.Count + 1);
+            string msg = "";
+            foreach (var item in nonNotifiableCollection)
+                msg += item.ToString() + "\n";
+            MessageBox.Show(msg);
+        }
+
+        public void AddNotifiableCollection()
+        {
+            notifiableCollection.Add(notifiableCollection.Count + 1);
+        }
+
+    }
+}
---
+++ WpfBindingCollectionsApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBindingCollectionsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DataSource();
+        }
+
+        private void NonNotifiableCollection_Click(object sender, RoutedEventArgs e)
+        {
+            ((DataSource)this.DataContext).AddNonNotifiableCollection();
+        }
+
+        private void NotifiableCollection_Click(object sender, RoutedEventArgs e)
+        {
+            ((DataSource)this.DataContext).AddNotifiableCollection();
+        }
+    }
+}
